7f71b613c8afd450cf9c818a4eeefe8c47a1dcd6
Jade Kim <JadeKim.dev@gmail.com>
2022-10-20 00:50:43 +0900

    HATEOAS implementation

--- src/main/java/list/todo/controller/TaskController.java
+++ src/main/java/list/todo/controller/TaskController.java
@@ -1,11 +1,12 @@
 package list.todo.controller;
 
-import list.todo.dto.MultiResponseDto;
-import list.todo.dto.SingleResponseDto;
+import list.todo.assembler.TaskModelAssembler;
 import list.todo.dto.TaskDto;
 import list.todo.entity.Task;
 import list.todo.mapper.TaskMapper;
 import list.todo.service.TaskService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @CrossOrigin
 @RequestMapping("/")
@@ -23,46 +27,49 @@ public class TaskController {
     private final TaskMapper mapper;
     private final TaskService taskService;
 
-    public TaskController(TaskMapper mapper, TaskService taskService) {
+    private final TaskModelAssembler assembler;
+
+    public TaskController(TaskMapper mapper, TaskService taskService, TaskModelAssembler assembler) {
         this.mapper = mapper;
         this.taskService = taskService;
+        this.assembler = assembler;
     }
 
     @PostMapping
-    public ResponseEntity postTask(@Valid @RequestBody TaskDto.Post requestBody) {
+    public ResponseEntity<EntityModel<Task>> postTask(@Valid @RequestBody TaskDto.Post requestBody) {
         Task task = mapper.taskPostDtoToTask(requestBody);
         Task createdTask = taskService.createTask(task);
-        TaskDto.Response response = mapper.taskToTaskResponse(createdTask);
+//        TaskDto.Response response = mapper.taskToTaskResponse(createdTask);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
+        return new ResponseEntity<>(assembler.toModel(createdTask), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity getTasks() {
+    public CollectionModel<Task> getTasks() {
         List<Task> tasks = taskService.findTasks();
-        List<TaskDto.Response> responses = mapper.taskToTaskResponses(tasks);
+//        List<TaskDto.Response> responses = mapper.taskToTaskResponses(tasks);
 
-        return new ResponseEntity<>(new MultiResponseDto<>(responses), HttpStatus.OK);
+        return CollectionModel.of(tasks, linkTo(methodOn(TaskController.class).getTasks()).withSelfRel());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getTask(@PathVariable int id) {
+    public ResponseEntity<EntityModel<Task>> getTask(@PathVariable int id) {
         Task task = taskService.findTask(id);
-        TaskDto.Response response = mapper.taskToTaskResponse(task);
+//        TaskDto.Response response = mapper.taskToTaskResponse(task);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+        return new ResponseEntity<>(assembler.toModel(task), HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity patchTask(@PathVariable int id
+    public ResponseEntity<EntityModel<Task>> patchTask(@PathVariable int id
             , @Valid @RequestBody TaskDto.Patch requestBody) {
 
         requestBody.setId(id);
         Task task = mapper.taskPatchToTask(requestBody);
         Task taskUpdated = taskService.updateTask(task);
-        TaskDto.Response response = mapper.taskToTaskResponse(taskUpdated);
+//        TaskDto.Response response = mapper.taskToTaskResponse(taskUpdated);
 
-        return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.OK);
+        return new ResponseEntity<>(assembler.toModel(taskUpdated), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
